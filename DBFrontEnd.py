fa415c1c15cb280b6b6d5e2f15102c2c3e095ad9
Picklin <38864014+Picklin@users.noreply.github.com>
2022-05-01 08:45:45 +0200

    Add files via upload

--- DBFrontEnd.py
+++ DBFrontEnd.py
@@ -2,12 +2,13 @@
 import gradio as gr
 import pandas as pd
 import datetime
+import preprocessing
+from inference_single import inference
 
 #Per obtenir l'ID mes gran i sumarli 1 per obtindre un de nou 
 #Es te per suposat que l'ID sempre comença per "RR" 
 sales = pd.read_csv('/home/martini/Escritorio/Vscode/sales.csv')
 max_id = sales['ID'].max()
-
 
 #interficie on es ficaran les dades
 textbox1 = gr.inputs.Textbox("text", label="PRODUCT ID", placeholder= "Product ID ...")
@@ -16,7 +17,7 @@ textbox3 = gr.inputs.Textbox("text", label="LOCATION", placeholder= "Location (n
 textbox4 = gr.inputs.Textbox("text", label="DATE", placeholder= "Date (yyyy-mm-dd) ...")
 textbox5 = gr.outputs.Label(type="auto", label="PREDICTION")
     
-#donem per suposat que la data esta be
+#donem per suposat que la Date esta be
 def stonks(Product_ID, Price, Location, Date): 
     price = float(Price)
     global max_id
@@ -34,11 +35,12 @@ def stonks(Product_ID, Price, Location, Date):
 
     database = pd.read_csv('/home/martini/Escritorio/Vscode/sku.csv')
    
+    
     table=pd.merge(product, database, on='SKU')
     if len(table) == 0:
         return 'No es troba el producte'
-    return f"Product ID: {Product_ID}\nPrice: {'%.2f'%price} €\nLocation: {Location}\nDate: {Date}\nID: {max_id}"
-   
+    return f"Product ID: {Product_ID}\nPrice: {'%.2f'%price} €\nLocation: {Location}\nDate: {Date}\nID: {max_id} \nSales: {inference('frozen_model.tf', preprocessing(product))}"
+    
 
 demo = gr.Interface(
     fn=stonks,
